9a4b07bf998afb9353ed445c82711eab5f6205a7
seadog007 <007seadog@gmail.com>
2014-11-27 14:58:49 +0800

    加入靜態頁面

---
+++ dist/footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+  <footer style="width: 728px;margin-left: auto;margin-right: auto">
+    <div class="row">
+    <?php if(file_exists('custom_footer.php'))
+    include 'custom_footer.php'; ?>
+    <p>這個救援的程式是由<a href="http://about.me/seadog007">海豹</a>所製作。<br>
+    前往<a href="https://github.com/YSITD/">我們的GitHub專案列表</a>來了解更多！<br>
+    本程式所有部份皆採：<a href="http://opensource.org/licenses/MIT">The MIT License</a>（MIT 許可證）授權，你可以依照授權條款來對本軟體修改、編輯、轉載…等。（文件部份採<a href="https://creativecommons.org/licenses/by-sa/3.0/tw/">CC BY-SA 3.0</a>）<br>
+    不過，我們更希望你可以前往<a href="https://github.com/YSITD/backer-of-baha">我們的GitHub專案</a>來進行改良、或是提出建議！<br>
+    這個頁面上的廣告將會被用於改善本服務！<br>
+    大家也可以到<a href="http://webchat.freenode.net/?channels=%23ysitd">Freenode IRC上的#ysitd</a>一起跟我們交流、切磋！<br>
+    <br><br>
+    聯絡我們：<a href="mailto:007seadog@gmail.com">007seadog@gmail.com</a></p>
+    </div>
+  </footer>
