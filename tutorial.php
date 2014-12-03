e5d4897ef228b0ab0a7b51f42774dc4a371f7180
seadog007 <007seadog@gmail.com>
2014-12-03 15:24:39 +0800

    加入空白的範本

---
+++ tutorial.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include_once("includes/head.php") ?>
+</head>
+<body id="home">
+  <div class="ui inverted masthead segment">
+    <div class="ui page grid">
+      <div class="column">
+        <?php include_once("dist/navbar.php") ?>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="ui vertical feature segment">
+    <div class="ui centered page grid">
+      <div class="fourteen wide column">
+        <div class="ui three column center aligned stackable divided grid">
+          <div class="column">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php include_once("dist/footer.php") ?>
+</body>
+
+</html>
