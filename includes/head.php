f435e0a2cf0c2114785887bffdc11a56ef23a330
seadog007 <007seadog@gmail.com>
2014-12-03 15:51:42 +0800

    bob.*加入共用head

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,9 @@
- <meta charset="utf-8" />
- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
- <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
- <title>巴哈文章救援&備份器 - Backer of Bahamut</title>
- <link rel="stylesheet" type="text/css" href="dist/css/semantic.css">
- <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.js"></script>
- <script src="dist/js/semantic.js"></script>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<title>巴哈文章救援&備份器 - Backer of Bahamut</title>
+<link rel="stylesheet" type="text/css" href="dist/css/semantic.css">
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.js"></script>
+<script src="dist/js/semantic.js"></script>
+<link rel="stylesheet" type="text/css" href="dist/css/bob.css">
+<script src="dist/js/bob.js"></script>
