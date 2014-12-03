c11019c8e9748a02d936bfd566320ad801a5a78c
seadog007 <007seadog@gmail.com>
2014-12-03 12:09:24 +0800

    加入navbar.php

---
+++ dist/navbar.php
@@ -0,0 +1,17 @@
+<div class="ui inverted blue menu">
+  <div class="header item">Backer of Bahamut</div>
+    <div class="right menu">
+      <div class="ui mobile dropdown link item">
+          Menu
+        <i class="dropdown icon"></i>
+        <div class="menu">
+          <a class="item" href="use.php">立即復原</a>
+          <a class="item">快速教學</a>
+          <a class="item" href="https://github.com/YSITD/backer-of-baha">此專案的Github</a>
+        </div>
+      </div>
+    <a class="item" href="use.php">立即復原</a>
+    <a class="item">快速教學</a>
+    <a class="item" href="https://github.com/YSITD/backer-of-baha">此專案的Github</a>
+  </div>
+</div>
