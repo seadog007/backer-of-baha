462654ae612a25bafe7697493d6fed7b37871ad1
seadog007 <007seadog@gmail.com>
2014-12-03 15:16:02 +0800

    加入共用head

---
+++ includes/head.php
@@ -0,0 +1,7 @@
+ <meta charset="utf-8" />
+ <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+ <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+ <title>巴哈文章救援&備份器 - Backer of Bahamut</title>
+ <link rel="stylesheet" type="text/css" href="dist/css/semantic.css">
+ <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.js"></script>
+ <script src="dist/js/semantic.js"></script>
