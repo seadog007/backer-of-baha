92e2fd39cba294b497edd8c96d3cd347da1a4a9b
seadog007 <007seadog@gmail.com>
2014-12-03 12:02:13 +0800

    加入footer

---
+++ dist/footer.php
@@ -0,0 +1,42 @@
+<div class="ui inverted footer vertical segment">
+    <div class="ui stackable center aligned page grid">
+      <div class="ten wide column">
+        <div class="ui three column center aligned stackable grid">
+          <div class="column">
+            <h5 class="ui inverted header">網站功能</h5>
+            <div class="ui inverted link list">
+              <a class="item" href="use.php">立即復原</a>
+              <a class="item">快速教學</a>
+              <a class="item">我們的Github</a>
+            </div>
+          </div>
+          <div class="column">
+            <h5 class="ui inverted header">所用技術</h5>
+            <div class="ui inverted link list">
+              <a class="item">Server：Wamp</a>
+              <a class="item" href="http://php.net/">Language：PHP</a>
+              <a class="item" href="http://semantic-ui.com/">Web UI：Semantic UI</a>
+            </div>
+          </div>
+          <div class="column">
+            <h5 class="ui inverted header">更多社群</h5>
+            <div class="ui inverted link list">
+              <a class="item">YSTID</a>
+              <a class="item" href="http://sitcon.org">SITCON</a>
+              <a class="item" href="http://g0v.tw">G0v</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="six wide column">
+        <h5 class="ui inverted header">聯絡我們</h5>
+        <p>
+          #ysitd@freenote.net<br>
+          目前無實體聚會<br>
+          補行~~~<br>
+          這是第四行~~~
+        </p>
+      </div>
+   </div>
+</div>
+<?php include_once("../includes/analyticstracking.php") ?>
